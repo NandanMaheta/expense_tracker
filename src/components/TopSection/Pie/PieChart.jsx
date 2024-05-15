bcce3cff54845ec7db270adbca78d068ceda66c1
NandanMaheta <nandzmehta99@gmail.com>
2024-05-15 19:20:24 +0530

    trying to make pie chart responsvie

--- src/components/TopSection/Pie/PieChart.jsx
+++ src/components/TopSection/Pie/PieChart.jsx
@@ -30,7 +30,7 @@ export default class PieComponent extends PureComponent {
 
   render() {
     return (
-        <div> <PieChart width={199} height={199}>
+        <div className={styles.PieChart}> <PieChart width={199} height={199}>
         <Pie
           data={data}
           cx={100}
